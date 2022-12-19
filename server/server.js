b4e2a75431f9cea4a8e72ac069429ec192bbf6c4
gvgui <gvgui@asu.edu>
2022-12-18 23:11:21 -0500

    added an express route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ const express = require("express")
 const app = express()
 
 const port = process.env.PORT || 3001;
+
+app.get("/getRestaurants", (req, res) => {
+    res.json ({
+        status: "success",
+        restaurant: "mcdonalds",
+    });
+});
+
 app.listen(port, () => {
     console.log(`server is up and listening on port ${port}`);
 });
