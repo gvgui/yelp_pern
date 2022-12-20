3d67e2f77e13284e255d38dd2da9ac3e4318016b
gvgui <gvgui@asu.edu>
2022-12-20 01:55:19 -0500

    finished creating route handling for update and delete functions

--- server/server.js
+++ server/server.js
@@ -1,13 +1,8 @@
-const express = require("express")
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
 
-const app = express()
-
-const port = process.env.PORT || 3001;
-
-app.use((req, res, next) => {
-    console.log("middleware runs");
-    next();
-});
+app.use((express.json()));
 
 //Gets All Restaurants
 app.get("/api/v1/restaurants", (req, res) => {
@@ -23,13 +18,45 @@ app.get("/api/v1/restaurants", (req, res) => {
 //Gets 1 Restaurant
 app.get("/api/v1/restaurants/:id", (req, res) => {
     console.log(req.params);
+    res.status(200).json ({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    });
 });
 
 //Creates a Restaurant
 app.post("/api/v1/restaurants", (req, res) => {
-    console.log(req);
+    console.log(req.body);
+    res.status(201).json ({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    });
 });
 
+//Update a Restaurant
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.status(200).json ({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds"
+        }
+    });
+});
+
+//Delete a Restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "success"
+    });
+});
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`server is up and listening on port ${port}`);
 });
