8eee2ffb6fa3d718fb647d4f9585a00aec75c69f
gvgui <gvgui@asu.edu>
2022-12-20 04:43:28 -0500

    react routing is set up for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
 
 const App = () => {
-    return <div>APP</div>;
+    return <div>
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home/>} />
+                <Route
+                path="/restaurants/:id/update"
+                element={<UpdatePage/>}
+                />
+                <Route
+                path="/restaurants/:id"
+                element={<RestaurantDetailPage/>}
+                />
+            </Routes>
+        </Router>
+    </div>;
 };
 
 export default App;
