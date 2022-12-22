3b564685b7bc85bf32c4dfbf22c40fbda5743139
gvgui <gvgui@asu.edu>
2022-12-21 23:38:39 -0500

    restaurant list now renders data from postgres db

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -37,7 +37,18 @@ const RestaurantList = (props) => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                {restaurants && restaurants.map((restaurant) => {
+                    return (
+                    <tr key={restaurant.id}>
+                        <td>{restaurant.name}</td>
+                        <td>{restaurant.location}</td>
+                        <td>{"$".repeat(restaurant.price_range)}</td>
+                        <td>reviews</td>
+                        <td><button className="btn btn-warning">Update</button></td>
+                        <td><button className="btn btn-danger">Delete</button></td>
+                    </tr>);
+                })}
+                {/*<tr>
                     <td>mcdonalds</td>
                     <td>new jersey</td>
                     <td>$</td>
@@ -52,7 +63,7 @@ const RestaurantList = (props) => {
                     <td>Rating</td>
                     <td><button className="btn btn-warning">Update</button></td>
                     <td><button className="btn btn-danger">Delete</button></td>
-                </tr>
+                </tr>*/}
             </tbody>
         </table>
     </div>
