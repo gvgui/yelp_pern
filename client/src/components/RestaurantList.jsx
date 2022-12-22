77beabfc34508e58001a1cddf709c0da66ded89f
gvgui <gvgui@asu.edu>
2022-12-22 00:29:22 -0500

    delete restaurant function now works

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -23,6 +23,17 @@ const RestaurantList = (props) => {
         fetchData();
     },[])
 
+    const handleDelete = async (id) => {
+        try {
+            const response = await RestaurantFinder.delete(`/${id}`);
+            setRestaurants(restaurants.filter(restaurant => {
+                return restaurant.id !== id
+            }))
+        } catch(err) {
+
+        }
+    }
+
   return (
     <div className='list-group'>
         <table className="table-hover table-dark">
@@ -45,7 +56,7 @@ const RestaurantList = (props) => {
                         <td>{"$".repeat(restaurant.price_range)}</td>
                         <td>reviews</td>
                         <td><button className="btn btn-warning">Update</button></td>
-                        <td><button className="btn btn-danger">Delete</button></td>
+                        <td><button onClick={() => handleDelete(restaurant.id)} className="btn btn-danger">Delete</button></td>
                     </tr>);
                 })}
                 {/*<tr>
