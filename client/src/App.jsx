421f133e781f3b8ed6c74c6cf6b806371442ac69
gvgui <gvgui@asu.edu>
2022-12-21 21:21:40 -0500

    added restaurants context and wrapped app component in it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,28 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./routes/Home";
 import UpdatePage from "./routes/UpdatePage";
 import RestaurantDetailPage from './routes/RestaurantDetailPage';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 
 const App = () => {
-    return <div className='container'>
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Home/>} />
-                <Route
-                path="/restaurants/:id/update"
-                element={<UpdatePage/>}
-                />
-                <Route
-                path="/restaurants/:id"
-                element={<RestaurantDetailPage/>}
-                />
-            </Routes>
-        </Router>
-    </div>;
+    return (
+        <RestaurantsContextProvider>
+            <div className='container'>
+                <Router>
+                    <Routes>
+                        <Route exact path="/" element={<Home/>} />
+                        <Route
+                        path="/restaurants/:id/update"
+                        element={<UpdatePage/>}
+                        />
+                        <Route
+                        path="/restaurants/:id"
+                        element={<RestaurantDetailPage/>}
+                        />
+                    </Routes>
+                </Router>
+            </div>
+        </RestaurantsContextProvider>
+    ) 
 };
 
 export default App;
