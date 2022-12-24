31b6e4a0b06b7430f70023ce70a0b780251162db
gvgui <gvgui@asu.edu>
2022-12-23 22:02:34 -0500

    update button now navigates to update restaurant page with current data loaded into input fields

---
+++ client/src/components/UpdateRestaurant.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useContext, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantsContext } from '../context/RestaurantsContext';
+
+const UpdateRestaurant = () => {
+  const {id} = useParams();
+  //const {restaurants} = useContext(RestaurantsContext);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("");
+
+  useEffect(() =>{
+    const fetchData = async () => {
+        const response = await RestaurantFinder.get(`/${id}`);
+        setName(response.data.data.restaurant.name);
+        setLocation(response.data.data.restaurant.location);
+        setPriceRange(response.data.data.restaurant.price_range);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+        <form action="">
+            <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input value={name} onChange={e => setName(e.target.value)} type="text" className="form-control" id="name" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="location">Location</label>
+                <input value={location} onChange={e => setLocation(e.target.value)} type="text" className="form-control" id="location" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="price_range">Price Range</label>
+                <select value={priceRange} onChange={e => setPriceRange(e.target.value)} className='custom-select' id="price_range">
+                    <option disabled>Price Range</option>
+                    <option value="1">$</option>
+                    <option value="2">$$</option>
+                    <option value="3">$$$</option>
+                    <option value="4">$$$$</option>
+                    <option value="5">$$$$$</option>
+                </select>
+            </div>
+            <button className='btn btn-primary'>Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default UpdateRestaurant
