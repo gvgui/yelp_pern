b922334b5bb4f3c8c7be2e8f7c4e2755a47ea892
gvgui <gvgui@asu.edu>
2022-12-23 23:48:38 -0500

    started the view for the restaurant detail page

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantsContext } from '../context/RestaurantsContext';
 
 const RestaurantDetailPage = () => {
+  const {id} = useParams();
+  const {selectedRestaurant, setSelectedRestaurant} = useContext(RestaurantsContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        setSelectedRestaurant(response.data.data.restaurant);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchData();
+  }, [])
   return (
     <div>
-        RestaurantDetailPage
+        <h1 className="text-center">{selectedRestaurant && selectedRestaurant.name}</h1>
     </div>
   )
 }
