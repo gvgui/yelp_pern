27f6db891d5fdb228fd61138aec3f5d27abb756e
gvgui <gvgui@asu.edu>
2022-12-26 21:20:03 -0500

    changed get a restaurant api to also retrieve reviews for the restaurant

--- server/server.js
+++ server/server.js
@@ -29,14 +29,16 @@ app.get("/api/v1/restaurants", async (req, res) => {
     }
 });
 
-//Gets 1 Restaurant
+//Gets 1 Restaurant + Reviews
 app.get("/api/v1/restaurants/:id", async (req, res) => {
     try {
-        const results = await db.query(`SELECT * FROM restaurants WHERE id = $1`, [req.params.id]);
+        const restaurant = await db.query(`SELECT * FROM restaurants WHERE id = $1`, [req.params.id]);
+        const reviews = await db.query(`SELECT * FROM reviews WHERE id = $1`, [req.params.id]);
         res.status(200).json ({
             status: "success",
             data: {
-                restaurant: results.rows[0]
+                restaurant: restaurant.rows[0],
+                reviews: reviews.rows
             },
         })
     } catch(err) {
