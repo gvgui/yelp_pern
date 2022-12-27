cb8af3c9fa03970b89f659e43686340090c805e6
gvgui <gvgui@asu.edu>
2022-12-26 21:43:12 -0500

    fixed api call to search for restaurant_id when searching for reviews

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.get("/api/v1/restaurants", async (req, res) => {
 app.get("/api/v1/restaurants/:id", async (req, res) => {
     try {
         const restaurant = await db.query(`SELECT * FROM restaurants WHERE id = $1`, [req.params.id]);
-        const reviews = await db.query(`SELECT * FROM reviews WHERE id = $1`, [req.params.id]);
+        const reviews = await db.query(`SELECT * FROM reviews WHERE restaurant_id = $1`, [req.params.id]);
         res.status(200).json ({
             status: "success",
             data: {
